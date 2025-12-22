2e043202a00342812b355473f2656af4af567cff
Jashwanth G <codesjash@gmail.com>
2025-12-22 21:35:14 +0530

    done

---
+++ forloop_practice/practice5.java
@@ -0,0 +1,15 @@
+public class practice5 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 0; i <= 100; i++) {
+
+            sum += i;
+        }
+
+        System.out.println("The sum of first 100 natural numbers is: " + sum);
+
+    }
+}
---
+++ forloop_practice/practice6.java
@@ -0,0 +1,15 @@
+public class practice6 {
+
+    public static void main(String[] args) {
+        
+        int fact = 1;
+        int n = 5;
+
+        for (int i = 1; i <= n;i++){
+            fact *= i;
+        }
+
+        System.out.println("The factorial of " + n + " is " + fact);
+    }
+    
+}
