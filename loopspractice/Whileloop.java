422a4d13f8733d904785c26a399cc4b0167cdbe9
Jashwanth G <codesjash@gmail.com>
2025-12-29 20:08:43 +0530

    loops

---
+++ loopspractice/Whileloop.java
@@ -0,0 +1,17 @@
+public class Whileloop {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int s = 0;
+
+        while (a <= 10) {
+
+            s = s + a;
+
+            System.out.println(s);
+
+            a++;
+        }
+    }
+}
